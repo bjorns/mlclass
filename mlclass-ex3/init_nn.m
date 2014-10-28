577627b219335d572f4e52c91be287a4273b7453
Björn Skoglund <bjorn.skoglund@gmail.com>
2014-10-28 21:10:37 +0100

    Week 3 done.

---
+++ mlclass-ex3/init_nn.m
@@ -0,0 +1,7 @@
+% Load saved matrices from file
+load('ex3weights.mat');
+
+% The matrices Theta1 and Theta2 will now be in your Octave
+% environment
+% Theta1 has size 25 x 401
+% Theta2 has size 10 x 26
